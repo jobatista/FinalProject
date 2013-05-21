a9f3326134b27c0eaaed14ac5fa124dac0b1d156
jobatista <osiris711bat@hotmail.com>
2013-05-21 15:29:39 -0600

    Update Human.java

--- Human.java
+++ Human.java
@@ -1,62 +1,15 @@
 
 public class Human extends Biped{
 
-  public Human(String newName, String newGender, int newAge, float newWeight,
-			int startSpeed, int startAccelaration,
-			float startRunningDistance, float newBodyLiftRatio) {
-		super(newName, newGender, newAge, newWeight, startSpeed, startAccelaration,
-				startRunningDistance, newBodyLiftRatio);
-		// TODO Auto-generated constructor stub
+	public Human(String newName, String newGender, int newAge, float newWeight,
+			float newBodyLiftRatio) {
+		super(newName, newGender, newAge, newWeight, newBodyLiftRatio);
 	}
 	
-	/*
-	 * public Human(String newName, String newGender, int newAge, float newWeight,
-			int startSpeed, int startAccelaration,
-			float startRunningDistance, float newBodyLiftRatio) {
-		super(newName, newGender, newAge, newWeight, /*startSpeed, startAccelaration,
-				startRunningDistance, newBodyLiftRatio);
-		// TODO Auto-generated constructor stub
-	}*/
-
-	public void setName(String newName){
-		
-		name = newName;
+	public void talk1(){	
+		System.out.println("Human " + runningDistance + " " + accelaration);
+		System.out.println("I walked " + runningDistance + " ft at a speed of "
+		+ speed + " ft per min. \n");	
 	}
-	
-	public String getName(){
-		
-		return name;
-		
-	}
-	
-	public void talk(){	
-		
-		System.out.printf( " What would you like to name me?\n%s!\n", getName());
 			
-	}
-	
-public void setRunningDistance(float newRunningDistance){
-		
-		runningDistance = speed + 2;
-	}
-	
-	public void print(){
-		System.out.println("this is " + name + " i am a " + gender + " years old " + age 
-				+ " i weigh " + weight + " i run fast " + speed + "mph " + "accelarating " + accelaration 
-				+ " i run a distance of " + runningDistance + " my body lift ratio is " + bodyLiftRatio + "\n");
-	}
-	
-	public void printSpeed(){
-		System.out.println("I walked " + runningDistance + " ft at a speed of " + speed + " ft per min. \n"  );
-	}
-	
-	/*private String name;
-	private String gender;
-	private int age;
-	private float weight;
-	private float speed;
-	private int accelaration;
-	private float runningDistance;
-	private float bodyLiftRatio;*/
-	
 }
