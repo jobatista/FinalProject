0bee4df1d05d2e3e8360a1201d33f23b983ac485
jobatista <osiris711bat@hotmail.com>
2013-05-21 15:28:44 -0600

    Update Biped.java

--- Biped.java
+++ Biped.java
@@ -1,66 +1,70 @@
 
 public class Biped {
-  
+	
 	protected String name;
 	protected String gender;
 	protected int age;
 	protected float weight;
 	protected int speed;
 	protected int accelaration;
-	protected float runningDistance;
+	protected int runningDistance;
 	protected float bodyLiftRatio;
 	
+	private int decrement;
+	
 	public Biped(String newName, String newGender, int newAge, float newWeight,
-				int startSpeed, int startAccelaration, float startRunningDistance, float newBodyLiftRatio){
+				float newBodyLiftRatio){
 		name = newName;
 		gender = newGender;
 		age = newAge;
 		weight = newWeight;
-		speed = startSpeed;
-		accelaration = startAccelaration;
-		runningDistance = startRunningDistance;
 		bodyLiftRatio = newBodyLiftRatio;
 		
 	}
 	
-	public void walk(){
-		/*int n; //math.random()*1;
+	public void setAccelaration()
+	{
+		accelaration = 1 + (int) ( Math.random() * 10);
+	}
+	
+	public int getAccelaration()
+	{
+		return accelaration;
+	}
+	
+	void walk(){
 		
-		if(n=1)
-		{
-			if (m=1)
-			{
-				
-			}
-			else{
-				
-			}
-		}
-		else*/
-		/////////Most likely belongs in main();///////////////
-		 
-		// wf = wf + a(accelaration)
-		// v = d(distance/t(time)
-		speed = speed + accelaration;
+		runningDistance = runningDistance + accelaration;
+		//speed = runningDistance / time;
 		
 	}
 	
 	public void turn(){
-		//wlr = wlr + a
-		//v = d/t
-		////////////Look at Walk!!!!!!!!!!!!!////////////
-		speed = speed + accelaration;
+		
+		
+		runningDistance = runningDistance + accelaration + 10;
+		//speed = runningDistance/time;
 	}
 	
 	public void run(){
 		
-		//w = w + a*2		
-		speed = speed + accelaration;
+		runningDistance = runningDistance + accelaration;
+		//speed = runningDistance/time;
 	}
 	
-	public void collide(int decrement){
-		//w = w - decrease
-		speed = speed - decrement;
+	public void collide(){
+		decrement = 1 + (int) ( Math.random() * 3);
+
+		accelaration = -decrement;
+		
+		runningDistance = runningDistance + accelaration;
+		
+		//speed = runningDistance/time;
+	}
+	
+	public void printSpeed(){
+		System.out.println("accelaration " + accelaration + " speed " + speed 
+				+" running distance " + runningDistance);
 	}
 	
 	public void lift(int liftedObjectWeight){
@@ -68,14 +72,11 @@ public class Biped {
 	}
 	
 	public float distance(){
-		runningDistance = speed + 2;
 		return runningDistance;
 	}
-	/*public void print(){
-		System.out.println("this is " + name + "i am a " + gender + "years old" + age 
-				+ "i weigh" + weight + " i run fast" + speed + "mph" + "accelarating" + accelaration 
-				+ "i run a distance of" + runningDistance + "my body lift ratio is " + bodyLiftRatio);
-	}*/
-
+	public void talk(){//change to talk
+		System.out.println("This is " + name + " i am a " + age + " year old " + gender +"\n" 
+				+ " I weigh " + weight +"lbs. my body lift ratio is " + bodyLiftRatio);
+	}
 
 }
